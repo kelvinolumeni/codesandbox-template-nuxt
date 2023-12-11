88fc5448d7f13b451dd83d4b605a5aa635b822cf
Kelvin Chimenem Webilor <123259729+kelvinolumeni@users.noreply.github.com>
2023-12-11 06:39:04 +0100

    Update NuxtLogo.spec.js

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -7,3 +7,27 @@ describe('NuxtLogo', () => {
     expect(wrapper.vm).toBeTruthy()
   })
 })
+package main
+
+import (
+  "context"
+  "fmt"
+  "log"
+
+  "github.com/ethereum/go-ethereum/common"
+  "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+    client, err := ethclient.Dial("https://eth-mainnet.g.alchemy.com/v2/K65KESvPum3ui73Jk6wOxxREAh8a2buu")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Get the latest known block
+    block, err := client.BlockByNumber(context.Background(), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Latest block:", block.Number().Uint64())
+}
